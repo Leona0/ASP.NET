18ae869bc85069fe44efffd872b35fcce8a613ba
Leona0 <scales202000@gmail.com>
2019-06-24 17:57:33 +0800

    first_MVC留言板
    小留言板

---
+++ WebApplication2/WebApplication2/Controllers/MessageController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+using WebApplication2.Service;
+
+namespace WebApplication2.Controllers
+{
+    public class MessageController : Controller
+    {
+        messageDBService data = new messageDBService();
+
+
+        // GET: Message
+        public ActionResult Index()
+        {
+            return View(data.GetData());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string Article_title, string Content)
+        {
+            data.DBCrate(Article_title, Content);
+            //導向指定的Action
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Service/messageDBService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication2.Models;
+
+namespace WebApplication2.Service
+{
+    public class messageDBService
+    {
+        public WebApplication2.Models.messageEntities1 db = new Models.messageEntities1();
+
+        public List<Article> GetData()
+        {
+            return (db.Article.ToList());
+        }
+
+        public void  DBCrate(string Article_title , string Content)
+        {
+            Article NEWData = new Article();
+            NEWData.Title = Article_title;
+            NEWData.Content = Content;
+            NEWData.time = DateTime.Now;
+
+
+            db.Article.Add(NEWData);
+
+            db.SaveChanges();
+        }
+    }
+}
